65ac6c667eb9f033226f81edb3189a4de0a534e0
Gabriela Diniz <gabriela.diniz@newrizon.global>
2025-04-24 13:09:41 -0300

    feat(frontend): criando estrutura do core, definindo o service de autenticação, environment e guarda de rotas

---
+++ frontend/qatu/src/app/core/guards/auth.guard.ts
@@ -0,0 +1,1 @@
+// TODO: Implementar o guarda de rotas futuramente
---
+++ frontend/qatu/src/app/core/services/auth/auth.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthService {
+
+    constructor() { }
+    //   TODO: Implementar as requisições relacionadas à autenticação do usuário
+}
