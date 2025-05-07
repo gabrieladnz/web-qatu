74e51214335d4cddeb36abb3ea3ed14c4a46eaaf
Gabriela Diniz <gadisa10@outlook.com>
2025-05-07 01:37:09 -0300

    refac(frontend): reorganizando as subcategorias para componentização

--- frontend/qatu/src/app/shared/components/navbar/navbar.component.ts
+++ frontend/qatu/src/app/shared/components/navbar/navbar.component.ts
@@ -1,13 +1,18 @@
-//
+// Libs
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { ProductService } from '../../../core/services/product/product.service';
+import { RouterModule } from '@angular/router';
+
+// Interfaces
 import { Product } from '../../../core/services/product/product.interface';
+
+// Services
+import { ProductService } from '../../../core/services/product/product.service';
 
 @Component({
     selector: 'app-navbar',
-    imports: [CommonModule, FormsModule],
+    imports: [CommonModule, FormsModule, RouterModule],
     templateUrl: './navbar.component.html',
     styleUrl: './navbar.component.scss',
 })
@@ -19,7 +24,7 @@ export class NavbarComponent {
 
     protected async search() {
         const productList = await this.productService.getProducts({
-            query: this.searchValue,
+            title: this.searchValue,
         });
         this.products.emit(productList);
     }
