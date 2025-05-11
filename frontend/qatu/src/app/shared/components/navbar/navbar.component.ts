6b42b69635a22f3d2d2a736895990fda7ab339d1
Gabriela Diniz <gadisa10@outlook.com>
2025-05-11 17:32:17 -0300

    feat(frontend): criação, configuração e estilização do modal do carrinho de compras

--- frontend/qatu/src/app/shared/components/navbar/navbar.component.ts
+++ frontend/qatu/src/app/shared/components/navbar/navbar.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
 
 // Interfaces
 import { Product } from '../../../core/services/product/product.interface';
@@ -12,6 +13,9 @@ import { ProductService } from '../../../core/services/product/product.service';
 
 // Enums
 import { CategoryType } from './../../enums/category-type.enum';
+
+// Components
+import { ModalCartComponent } from '../modals/modal-cart/modal-cart.component';
 
 @Component({
     selector: 'app-navbar',
@@ -24,7 +28,7 @@ export class NavbarComponent {
     protected searchValue: string = '';
     protected CategoryType = CategoryType;
 
-    constructor(private router: Router) { }
+    constructor(private router: Router, public dialog: MatDialog) { }
 
     protected search(): void {
         // TODO: A lógica aqui vai ser parecida com a da função searchToCategory().
@@ -40,6 +44,18 @@ export class NavbarComponent {
     protected searchToCategory(category: string): void {
         this.router.navigate(['/search'], {
             queryParams: { category }
+        });
+    }
+
+    protected openModalCart(): void {
+        this.dialog.open(ModalCartComponent, {
+            width: '400px',
+            height: '100%',
+            panelClass: 'custom__modal',
+            disableClose: false,
+            position: {
+                right: '0'
+            },
         });
     }
 }
