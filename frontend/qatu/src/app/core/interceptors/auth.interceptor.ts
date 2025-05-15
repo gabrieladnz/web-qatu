579c82a377cd561a52e5c3ea35412778a438cffc
Gabriela Diniz <gadisa10@outlook.com>
2025-05-15 12:27:33 -0300

    fix(frontend): ajuste no interceptor para detectar o servidor offline

--- frontend/qatu/src/app/core/interceptors/auth.interceptor.ts
+++ frontend/qatu/src/app/core/interceptors/auth.interceptor.ts
@@ -8,9 +8,11 @@ import { catchError, throwError } from 'rxjs';
 import { TokenService } from '../services/token/token.service';
 
 export const hasAuthError = signal<boolean>(false);
+export const hasConnectionError = signal<boolean>(false);
 
 export const authInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn) => {
     const tokenService = inject(TokenService);
+    const snackBar = inject(MatSnackBar);
 
     return next(req).pipe(
         catchError((error: HttpErrorResponse) => {
@@ -18,6 +20,15 @@ export const authInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, ne
                 hasAuthError.set(true);
                 tokenService.delete();
             }
+
+            if (error.status === 0) {
+                hasConnectionError.set(true);
+                tokenService.delete();
+                snackBar.open('Não foi possível conectar ao servidor. Por favor, tente novamente mais tarde.', 'Fechar', {
+                    duration: 5000
+                });
+            }
+
             return throwError(() => error);
         })
     );
