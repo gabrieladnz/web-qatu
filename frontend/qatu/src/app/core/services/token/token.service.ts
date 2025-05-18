f68e26f862061b7e41d702cb47222120c25afe5f
RenatoCarv <renato.sa@jala.university>
2025-05-18 19:33:36 -0300

    fix(frontend): adiciona a dependência HttpClient ao TokenService para funcionalidade de logout com backend

--- frontend/qatu/src/app/core/services/token/token.service.ts
+++ frontend/qatu/src/app/core/services/token/token.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 @Injectable({
-    providedIn: 'root'
+    providedIn: 'root',
 })
 export class TokenService {
     private type: 'sessionStorage' | 'localStorage' = 'localStorage';
     private readonly tokenUser = 'auth_token';
 
-    constructor(private router: Router) { }
+    constructor(private router: Router, private http: HttpClient) {}
 
     public get(): string | null {
         return window[this.type].getItem(this.tokenUser);
@@ -23,6 +24,22 @@ export class TokenService {
     }
 
     public async logout(): Promise<boolean> {
+        const token = this.get();
+
+        try {
+            if (token) {
+                const headers = new HttpHeaders().set(
+                    'Authorization',
+                    `Bearer ${token}`
+                );
+                await firstValueFrom(
+                    this.http.post('/api/auth/logout', {}, { headers })
+                );
+            }
+        } catch (error) {
+            console.error('Erro ao fazer logout no backend:', error);
+        }
+
         this.delete();
         return this.router.navigate(['/auth/login'], {
             replaceUrl: true,
