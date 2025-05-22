3dbdd161baed57e6b08dba557eba47a108a804c7
RenatoCarv <renato.sa@jala.university>
2025-05-21 23:23:09 -0300

    feat(frontend): adiciona estrutura inicial dos componentes modais para contato, central de ajuda e termos de uso

---
+++ frontend/qatu/src/app/shared/components/modals/modal-help-center/modal-help-center.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-help-center',
+  imports: [],
+  templateUrl: './modal-help-center.component.html',
+  styleUrl: './modal-help-center.component.scss'
+})
+export class ModalHelpCenterComponent {
+
+}
