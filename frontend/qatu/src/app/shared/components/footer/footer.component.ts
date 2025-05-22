0755832d5ffc8a08679ce07ecc98dc826040514d
RenatoCarv <renato.sa@jala.university>
2025-05-21 23:16:15 -0300

    refactor(frontend): adicionar importações de diálogo e implementar a funcionalidade modal.

--- frontend/qatu/src/app/shared/components/footer/footer.component.ts
+++ frontend/qatu/src/app/shared/components/footer/footer.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { MatDialog, MatDialogModule  } from '@angular/material/dialog';
+import { ModalAboutQatuComponent } from '../../../shared/components/modals/modal-about-qatu/modal-about-qatu.component';
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule, MatDialogModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
 
+export class FooterComponent {
+  constructor(private dialog: MatDialog) {}
+
+  openAboutModal(): void {
+    this.dialog.open(ModalAboutQatuComponent, {
+      width: '800px',
+      maxWidth: '90vw',
+      panelClass: 'about-modal-container',
+      autoFocus: false
+    });
+  }
 }
