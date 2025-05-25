ed36cca752df4b401d9697a653f28309961fca82
RenatoCarv <renato.sa@jala.university>
2025-05-24 23:49:00 -0300

    feat(frontend): implementa modal de termos de uso com conteúdo e estilização padronizada

--- frontend/qatu/src/app/shared/components/modals/modal-terms-of-use/modal-terms-of-use.component.ts
+++ frontend/qatu/src/app/shared/components/modals/modal-terms-of-use/modal-terms-of-use.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-modal-terms-of-use',
-  imports: [],
-  templateUrl: './modal-terms-of-use.component.html',
-  styleUrl: './modal-terms-of-use.component.scss'
+    selector: 'app-modal-terms-of-use',
+    imports: [MatIconModule],
+    templateUrl: './modal-terms-of-use.component.html',
+    styleUrls: ['./modal-terms-of-use.component.scss'],
 })
 export class ModalTermsOfUseComponent {
+    constructor(private dialogRef: MatDialogRef<ModalTermsOfUseComponent>) {}
 
+    closeModal(): void {
+        this.dialogRef.close();
+    }
 }
