aea0a1d9bb3209155ba6532d90d1fb32d9a47544
RenatoCarv <renato.sa@jala.university>
2025-05-24 23:48:07 -0300

    feat(frontend): implementa o modal de contato com layout estruturado e estilização padronizada.

--- frontend/qatu/src/app/shared/components/modals/modal-contact/modal-contact.component.ts
+++ frontend/qatu/src/app/shared/components/modals/modal-contact/modal-contact.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatDialogRef } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
-  selector: 'app-modal-contact',
-  imports: [],
-  templateUrl: './modal-contact.component.html',
-  styleUrl: './modal-contact.component.scss'
+    selector: 'app-modal-contact',
+    standalone: true,
+    imports: [MatIconModule, CommonModule],
+    templateUrl: './modal-contact.component.html',
+    styleUrls: ['./modal-contact.component.scss'],
 })
 export class ModalContactComponent {
+    constructor(
+        private dialogRef: MatDialogRef<ModalContactComponent>,
+        private matIconRegistry: MatIconRegistry,
+        private domSanitizer: DomSanitizer
+    ) {
+        this.matIconRegistry.addSvgIcon(
+            'instagram',
+            this.domSanitizer.bypassSecurityTrustResourceUrl(
+                '../assets/figures/instagram.svg'
+            )
+        );
+    }
 
+    closeModal(): void {
+        this.dialogRef.close();
+    }
 }
