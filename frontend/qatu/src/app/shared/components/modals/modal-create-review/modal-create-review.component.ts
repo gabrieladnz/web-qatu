1b7a0faecae27a8c40b5f537f1330638f240794d
Gabriela Diniz <gadisa10@outlook.com>
2025-05-26 19:44:07 -0300

    feat(frontend): criando modal de criação de review

---
+++ frontend/qatu/src/app/shared/components/modals/modal-create-review/modal-create-review.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-create-review',
+  imports: [],
+  templateUrl: './modal-create-review.component.html',
+  styleUrl: './modal-create-review.component.scss'
+})
+export class ModalCreateReviewComponent {
+
+}
