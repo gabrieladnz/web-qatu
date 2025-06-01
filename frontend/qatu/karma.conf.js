235cd8abf0716304e0d0d24af9dec36cd24f3714
Marcelo Barbosa da Silva <marcelo.barbosa@jala.university>
2025-06-01 15:06:38 -0300

    Fix: Karma.conf.js e gitlab-ci.yml refatorados

--- frontend/qatu/karma.conf.js
+++ frontend/qatu/karma.conf.js
@@ -32,19 +32,32 @@ module.exports = function (config) {
     singleRun: true,
     restartOnFileChange: false,
     
+    // Usar configuração específica para CI
     browsers: process.env.CI ? ['ChromeHeadlessCINoSandbox'] : ['Chrome'],
+    
     customLaunchers: {
       ChromeHeadlessCINoSandbox: {
         base: 'ChromeHeadless',
         flags: [
           '--no-sandbox',
-          '--disable-gpu',
-          '--disable-dev-shm-usage',
           '--disable-setuid-sandbox',
+          '--disable-dev-shm-usage',
+          '--disable-gpu',
+          '--disable-web-security',
+          '--disable-features=VizDisplayCompositor',
+          '--remote-debugging-port=9222',
           '--headless',
-          '--disable-web-security'
+          '--disable-background-timer-throttling',
+          '--disable-backgrounding-occluded-windows',
+          '--disable-renderer-backgrounding'
         ]
       }
-    }
+    },
+    
+    // Configurações para ambiente CI
+    captureTimeout: 120000,
+    browserDisconnectTimeout: 10000,
+    browserDisconnectTolerance: 3,
+    browserNoActivityTimeout: 60000
   });
 };
