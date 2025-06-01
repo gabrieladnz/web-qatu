df4fb73b421e41955ef46cbb286953a84c1ff753
Marcelo Barbosa da Silva <marcelo.barbosa@jala.university>
2025-06-01 04:30:12 -0300

    Fix: Mudando imagem de testes de front de novo

--- frontend/qatu/karma.conf.js
+++ frontend/qatu/karma.conf.js
@@ -1,5 +1,3 @@
-process.env.CHROME_BIN = require('puppeteer').executablePath();
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -17,15 +15,45 @@ module.exports = function (config) {
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage'),
       subdir: '.',
-      reporters: [{ type: 'html' }, { type: 'text-summary' }],
+      reporters: [
+        { type: 'html' }, 
+        { type: 'text-summary' },
+        { type: 'lcovonly' },
+        { type: 'cobertura' }
+      ],
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
     browsers: ['ChromeHeadless'],
     singleRun: true,
-    restartOnFileChange: false
+    restartOnFileChange: false,
+    
+    // Configurações específicas para ambiente CI
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--disable-web-security',
+          '--disable-gpu',
+          '--disable-dev-shm-usage',
+          '--disable-extensions',
+          '--remote-debugging-port=9222',
+          '--headless'
+        ]
+      }
+    },
+    
+    // Usar ChromeHeadlessCI em ambiente CI
+    browsers: process.env.CI ? ['ChromeHeadlessCI'] : ['ChromeHeadless'],
+    
+    // Configurações de timeout para CI
+    browserDisconnectTimeout: 10000,
+    browserDisconnectTolerance: 3,
+    browserNoActivityTimeout: 60000,
+    captureTimeout: 60000
   });
 };
